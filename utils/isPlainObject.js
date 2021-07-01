098f4065b14ab6f554682b74ac618687c9ed46c8
倚窗听雨丿醉无心 <994039348@qq.com>
2021-07-01 09:26:46 +0800

    为 isPlainObject 工具方法添加示例

--- utils/isPlainObject.js
+++ utils/isPlainObject.js
@@ -1,7 +1,13 @@
 /**
  * 判断传入对象是否是纯粹的对象
+ * 
  * @param {any} obj 需要判断的对象
  * @returns {Boolean}
+ * @example
+ * 
+ * isPlainObject({}); // -> true
+ * isPlainObject(Object.create(null)); // -> true
+ * isPlainObject([]); // -> false
  */
 export default function isPlainObject(obj) {
   return Object.prototype.toString.call(obj) === '[object Object]';
