3706ed4711b08b1e4ef897bb7c15241347e34922
倚窗听雨丿醉无心 <994039348@qq.com>
2021-07-01 09:29:55 +0800

    为 isString 工具方法添加示例

--- utils/isString.js
+++ utils/isString.js
@@ -1,7 +1,12 @@
 /**
  * 判断传入对象是否是 String 类型
+ * 
  * @param {any} obj 需要判断的对象
  * @returns {Boolean}
+ * 
+ * isString('666'); // -> true
+ * isString(new String('666')); // -> true
+ * isString(666); // -> false
  */
 export default function isString(obj) {
   return typeof obj === 'string' || Object.prototype.toString.call(obj) === '[object String]'
