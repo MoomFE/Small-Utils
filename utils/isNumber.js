edf56467443f3d72d3b41c479a6a24b29e113a7c
倚窗听雨丿醉无心 <994039348@qq.com>
2021-07-01 09:26:54 +0800

    为 isNumber 工具方法添加示例

--- utils/isNumber.js
+++ utils/isNumber.js
@@ -1,7 +1,13 @@
 /**
  * 判断传入对象是否是 Number 类型, 并且不为 NaN
+ * 
  * @param {any} obj 需要判断的对象
  * @returns {Boolean}
+ * @example
+ * 
+ * isNumber(666); // -> true
+ * isNumber(NaN); // -> false
+ * isNumber('666'); // -> false
  */
 export default function isNumber(obj) {
   return typeof obj === 'number' && obj === obj; // eslint-disable-line no-self-compare
