b3bde4913dd5764a55c1c585059d1c83c50ff6f2
倚窗听雨丿醉无心 <994039348@qq.com>
2021-07-06 10:10:35 +0800

    配置项目 Eslint

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/essential',
+    './node_modules/@moomfe/eslint-config/.eslintrc.vue.js'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+};
