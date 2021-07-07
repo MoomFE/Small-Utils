13c17c14e2c293a9bb707c28c6ab7b6fb3fa72ab
倚窗听雨丿醉无心 <994039348@qq.com>
2021-07-07 16:50:29 +0800

    新增 SScrollbars 组件

---
+++ components/SScrollbars/index.vue
@@ -0,0 +1,14 @@
+<script>
+  import 'overlayscrollbars/css/OverlayScrollbars.min.css';
+  import { OverlayScrollbarsComponent } from 'overlayscrollbars-vue';
+
+  export default {
+    functional: true,
+    render(create, { data, children }) {
+      // 添加样式类
+      data.staticClass = `s-scrollbars ${data.staticClass || ''}`.trim();
+
+      return create(OverlayScrollbarsComponent, data, children);
+    }
+  };
+</script>
