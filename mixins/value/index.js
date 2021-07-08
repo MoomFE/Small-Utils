8e8e70532231e6b7d6ed0ffbbc22bc0f69b930d8
倚窗听雨丿醉无心 <994039348@qq.com>
2021-07-08 21:24:26 +0800

    修改注释

--- mixins/value/index.js
+++ mixins/value/index.js
@@ -3,23 +3,23 @@ import Vue from 'vue';
 
 export default Vue.extend({
   props: {
-    /** 组件值 ( 父组件传入 ) */
+    /** 父组件传入值 */
     value: null
   },
   data() {
     return {
-      /** 组件值 */
+      /** 当前组件值 */
       currentValue: this.value
     };
   },
   methods: {
-    /** 设置组件值 */
+    /** 设置当前组件值 */
     setValue(value) {
       this.currentValue = value;
     }
   },
   watch: {
-    // 父组件值更改后, 更改当前组件值
+    // 父组件传入值更改后, 更改当前组件值
     value(value) {
       this.currentValue = value;
     },
