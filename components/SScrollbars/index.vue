e057478507a44491478a6ab73be051b873144178
倚窗听雨丿醉无心 <994039348@qq.com>
2021-07-08 10:37:23 +0800

    修复在使用 s-scrollbars 组件时, 报错说 overlayscrollbars 没有 default 导出的问题
    修复在使用 s-scrollbars 组件时
    报错说 overlayscrollbars 没有 default 导出的问题

--- components/SScrollbars/index.vue
+++ components/SScrollbars/index.vue
@@ -1,14 +1,63 @@
+<template>
+  <div class="os-host s-scrollbars">
+    <div class="os-resize-observer-host" />
+    <div class="os-padding">
+      <div class="os-viewport">
+        <div class="os-content">
+          <slot />
+        </div>
+      </div>
+    </div>
+    <div class="os-scrollbar os-scrollbar-horizontal">
+      <div class="os-scrollbar-track">
+        <div class="os-scrollbar-handle" />
+      </div>
+    </div>
+    <div class="os-scrollbar os-scrollbar-vertical">
+      <div class="os-scrollbar-track">
+        <div class="os-scrollbar-handle" />
+      </div>
+    </div>
+    <div class="os-scrollbar-corner" />
+  </div>
+</template>
+
 <script>
-  import 'overlayscrollbars/css/OverlayScrollbars.min.css';
-  import { OverlayScrollbarsComponent } from 'overlayscrollbars-vue';
+  import 'overlayscrollbars/css/OverlayScrollbars.css';
+  import 'overlayscrollbars';
 
   export default {
-    functional: true,
-    render(create, { data, children }) {
-      // 添加样式类
-      data.staticClass = `s-scrollbars ${data.staticClass || ''}`.trim();
+    name: 's-scrollbars',
+    props: {
+      options: { type: Object },
+      extensions: { type: [String, Array, Object] }
+    },
+    data: () => ({
+      osInstace: null
+    }),
+    mounted() {
+      this.osInstace = window.OverlayScrollbars(
+        this.$el,
+        this.options || {},
+        this.extensions
+      );
+    },
+    beforeDestroy() {
+      const osInstance = this.osInstace;
 
-      return create(OverlayScrollbarsComponent, data, children);
+      if (window.OverlayScrollbars.valid(osInstance)) {
+        osInstance.destroy();
+        this.osInstace = null;
+      }
+    },
+    watch: {
+      options(options) {
+        const osInstance = this.osInstace;
+
+        if (window.OverlayScrollbars.valid(osInstance)) {
+          osInstance.options(options);
+        }
+      }
     }
   };
 </script>
