7d199c0a2958be7c688200753df181adf2b3f289
倚窗听雨丿醉无心 <994039348@qq.com>
2021-07-30 16:17:58 +0800

    perf(web): 移动 mixins 文件夹

--- components/SSelect/index.vue
+++ components/SSelect/index.vue
@@ -1,8 +1,8 @@
 <script>
   import VSelect from 'vuetify/lib/components/VSelect';
   import mixins from 'vuetify/lib/util/mixins';
-  import Value from '../../mixins/value/index';
-  import InputGenerate from '../../mixins/input-generate/index';
+  import Value from '../_mixins/value/index';
+  import InputGenerate from '../_mixins/input-generate/index';
 
 
   export default mixins(Value, InputGenerate).extend({
