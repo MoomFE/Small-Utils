3cfdc75d876dfe633dcf4468763fbeb470f2fad4
倚窗听雨丿醉无心 <994039348@qq.com>
2021-08-03 14:52:06 +0800

    修复 `s-textarea` 组件拖拽大小无效的问题

--- components/STextarea/components/Textarea.vue
+++ components/STextarea/components/Textarea.vue
@@ -20,44 +20,62 @@
       const inputWrap = templateRef('input-wrap');
       const scrollbars = templateRef('scrollbars');
 
+      /** 是否初始化过滚动条高度 */
+      let isInit = false;
       /** 滚动条容器最小高度 */
-      let scrollbarsMinHeight = parseInt(props.rows, 10) * parseFloat(props.rowHeight);
+      let minHeight = parseInt(props.rows, 10) * parseFloat(props.rowHeight);
+      /** 滚动条在可拖拽模式下的高度 */
+      let resizeHeight;
 
       // 监听文本域, 发生变动时, 调整文本域和滚动条容器的高度
       useResizeObserver(inputWrap, () => {
         const inputEl = input.value;
         const inputWrapEl = inputWrap.value;
         const scrollbarsEl = scrollbars.value.$el;
-        let size = 0;
+        let inputMarginTop = 0;
 
         // 启用了自动增长高度时
         // 文本域高度随着内容调整
         // 滚动条容器高度随着文本域占用高度调整
         if (props.autoGrow) {
           inputEl.style.height = '0';
-          inputEl.style.height = `${Math.max(scrollbarsMinHeight, inputEl.scrollHeight)}px`;
-          scrollbarsEl.style.height = `${Math.max(scrollbarsMinHeight, inputWrapEl.scrollHeight)}px`;
+          inputEl.style.height = `${Math.max(minHeight, inputEl.scrollHeight)}px`;
+          scrollbarsEl.style.height = `${Math.max(minHeight, inputWrapEl.scrollHeight)}px`;
         }
         // 未启用自动增长高度时
         // 文本域高度随着内容调整
-        // 滚动条容器高度默认使用滚动条容器最小高度
+        // 滚动条容器高度默认使用滚动条容器最小高度, 如果调整过滚动条容器大小, 那么使用调整过的滚动条容器大小
         else {
           // 获取文本域上边距
-          size = getComputedStyle(inputEl).marginTop;
+          inputMarginTop = getComputedStyle(inputEl).marginTop;
 
           // 首次计算时, 如果文本域占用高度大于滚动条容器最小高度, 那么将滚动条容器最小高度修改为文本域占用高度
-          if (!inputEl.style.height && (inputWrapEl.scrollHeight > scrollbarsMinHeight)) {
-            scrollbarsMinHeight = inputWrapEl.scrollHeight;
+          if (!inputEl.style.height && (inputWrapEl.scrollHeight > minHeight)) {
+            minHeight = inputWrapEl.scrollHeight;
           }
 
           inputEl.style.height = `${inputEl.scrollHeight}px`;
-          scrollbarsEl.style.height = `${scrollbarsMinHeight}px`;
+          scrollbarsEl.style.height = `${resizeHeight != null ? resizeHeight : minHeight}px`;
         }
 
-        // 修复文本域内容会和 label 重叠的问题
-        inputWrapEl.style.transform = `translateY(-${size})`;
-        scrollbars.value.$refs.viewport.style.borderTop = `${size} solid transparent`;
+        // 防止文本域内容会和 label 重叠的问题
+        inputWrapEl.style.transform = `translateY(-${inputMarginTop})`;
+        scrollbars.value.$refs.viewport.style.borderTop = `${inputMarginTop} solid transparent`;
+
+        // 标记已初始化
+        isInit = true;
       });
+
+      /**
+       * 当调整滚动条容器大小时, 记住滚动条容器大小
+       */
+      function scrollbarsOnHostSizeChanged({ height }) {
+        if (isInit) resizeHeight = height;
+      }
+
+      return {
+        scrollbarsOnHostSizeChanged
+      };
     },
     computed: {
       classes() {
@@ -74,7 +92,10 @@
           ref: 'scrollbars',
           props: {
             options: {
-              resize: this.noResizeHandle ? 'n' : 'v'
+              resize: this.noResizeHandle ? 'n' : 'v',
+              callbacks: {
+                onHostSizeChanged: this.scrollbarsOnHostSizeChanged
+              }
             }
           },
           staticStyle: {
