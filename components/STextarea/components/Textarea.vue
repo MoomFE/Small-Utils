644f925832bf33ec86a8510055e08757bde80775
倚窗听雨丿醉无心 <994039348@qq.com>
2021-08-03 11:37:36 +0800

    修复文本域内容会和 label 重叠的问题

--- components/STextarea/components/Textarea.vue
+++ components/STextarea/components/Textarea.vue
@@ -2,7 +2,7 @@
   /* eslint-disable brace-style */
 
 
-  import { defineComponent, ref, h as create } from '@vue/composition-api';
+  import { defineComponent, h as create } from '@vue/composition-api';
   import { templateRef, useResizeObserver } from '@vueuse/core';
   import VTextField from 'vuetify/lib/components/VTextField/VTextField';
   import VTextarea from 'vuetify/lib/components/VTextarea/VTextarea';
@@ -18,42 +18,46 @@
     setup(props) {
       const input = templateRef('input');
       const inputWrap = templateRef('input-wrap');
+      const scrollbars = templateRef('scrollbars');
 
-      /** 滚动条容器高度 */
-      const scrollbarsHeight = ref('auto');
       /** 滚动条容器最小高度 */
       let scrollbarsMinHeight = parseInt(props.rows, 10) * parseFloat(props.rowHeight);
 
       // 监听文本域, 发生变动时, 调整文本域和滚动条容器的高度
       useResizeObserver(inputWrap, () => {
-        const el = input.value;
-        const elWrap = inputWrap.value;
+        const inputEl = input.value;
+        const inputWrapEl = inputWrap.value;
+        const scrollbarsEl = scrollbars.value.$el;
+        let size = 0;
 
         // 启用了自动增长高度时
         // 文本域高度随着内容调整
         // 滚动条容器高度随着文本域占用高度调整
         if (props.autoGrow) {
-          el.style.height = '0';
-          el.style.height = `${Math.max(scrollbarsMinHeight, el.scrollHeight)}px`;
-          scrollbarsHeight.value = `${Math.max(scrollbarsMinHeight, elWrap.scrollHeight)}px`;
+          inputEl.style.height = '0';
+          inputEl.style.height = `${Math.max(scrollbarsMinHeight, inputEl.scrollHeight)}px`;
+          scrollbarsEl.style.height = `${Math.max(scrollbarsMinHeight, inputWrapEl.scrollHeight)}px`;
         }
         // 未启用自动增长高度时
         // 文本域高度随着内容调整
         // 滚动条容器高度默认使用滚动条容器最小高度
         else {
+          // 获取文本域上边距
+          size = getComputedStyle(inputEl).marginTop;
+
           // 首次计算时, 如果文本域占用高度大于滚动条容器最小高度, 那么将滚动条容器最小高度修改为文本域占用高度
-          if (!el.style.height && (elWrap.scrollHeight > scrollbarsMinHeight)) {
-            scrollbarsMinHeight = elWrap.scrollHeight;
+          if (!inputEl.style.height && (inputWrapEl.scrollHeight > scrollbarsMinHeight)) {
+            scrollbarsMinHeight = inputWrapEl.scrollHeight;
           }
 
-          el.style.height = `${el.scrollHeight}px`;
-          scrollbarsHeight.value = `${scrollbarsMinHeight}px`;
+          inputEl.style.height = `${inputEl.scrollHeight}px`;
+          scrollbarsEl.style.height = `${scrollbarsMinHeight}px`;
         }
-      });
 
-      return {
-        scrollbarsHeight
-      };
+        // 修复文本域内容会和 label 重叠的问题
+        inputWrapEl.style.transform = `translateY(-${size})`;
+        scrollbars.value.$refs.viewport.style.borderTop = `${size} solid transparent`;
+      });
     },
     computed: {
       classes() {
@@ -75,8 +79,7 @@
           },
           staticStyle: {
             'align-self': 'stretch',
-            'width': '100%',
-            'height': this.scrollbarsHeight
+            'width': '100%'
           }
         };
 
