bf9629734edcbe00408abfd8bea72427e246baba
倚窗听雨丿醉无心 <994039348@qq.com>
2021-08-11 11:26:52 +0800

    使用组合式 API 重写 `s-date-picker` 组件

--- components/SDatePicker/index.vue
+++ components/SDatePicker/index.vue
@@ -1,81 +1,75 @@
 <script>
-  import { defineComponent, h as create } from '@vue/composition-api';
+  import { defineComponent, h as create, ref } from '@vue/composition-api';
   import VMenu from 'vuetify/lib/components/VMenu/VMenu';
   import VDatePicker from 'vuetify/lib/components/VDatePicker/VDatePicker';
+  import mergeData from 'vuetify/lib/util/mergeData';
+  import { entries } from 'lodash';
   import SInput from '../SInput/index.vue';
   import useValue from '../_composables/useValue';
+  import useInputGenerate from '../_composables/useInputGenerate';
+
 
   export default defineComponent({
     name: 's-date-picker',
     setup(props, ctx) {
       const { lazyValue, internalValue, setValue } = useValue(props, ctx);
+      const { getInputData } = useInputGenerate(props, ctx, { internalValue, setValue });
 
-      return {
-        lazyValue, internalValue, setValue
-      };
-    },
-    data: () => ({
       /** 菜单激活状态 */
-      isMenuActive: false
-    }),
-    render() {
-      return create(VMenu, {
+      const isMenuActive = ref(false);
+
+      /**
+       * 生成组件的输入框部分
+       */
+      function genInput({ attrs, on }) {
+        const data = mergeData(getInputData(), {
+          on,
+          attrs,
+          props: {
+            readonly: true
+          }
+        });
+        const children = entries(ctx.slots).map(([slot, fn]) => {
+          return create('template', { slot }, fn());
+        });
+
+        return create(SInput, data, children);
+      }
+
+      /**
+       * 生成组件的日期选择器部分
+       */
+      function genDatePicker() {
+        return create(VDatePicker, {
+          props: {
+            value: internalValue.value
+          },
+          on: {
+            input: [
+              setValue,
+              () => (isMenuActive.value = false)
+            ]
+          }
+        });
+      }
+
+      return () => create(VMenu, {
         ref: 'menu',
         props: {
-          value: this.isMenuActive,
+          value: isMenuActive.value,
           offsetY: true,
           closeOnContentClick: false,
           transition: 'scale-transition',
           minWidth: 'auto'
         },
         on: {
-          input: (value) => (this.isMenuActive = value)
+          input: (value) => (isMenuActive.value = value)
         },
         scopedSlots: {
-          activator: this.genInput
+          activator: genInput,
+          default: genDatePicker
         }
-      }, [
-        this.genDatePicker()
-      ]);
-    },
-    methods: {
-      /**
-       * 生成组件的输入框部分
-       */
-      genInput({ attrs, on }) {
-        return create(SInput, {
-          attrs: {
-            ...attrs
-          },
-          props: {
-            value: this.internalValue,
-            readonly: true
-          },
-          on: {
-            input: this.setValue
-          },
-          nativeOn: {
-            ...on
-          }
-        });
-      },
-      /**
-       * 生成组件的日期选择器部分
-       */
-      genDatePicker() {
-        return create(VDatePicker, {
-          props: {
-            value: this.internalValue,
-            scrollable: true
-          },
-          on: {
-            input: [
-              this.setValue,
-              () => (this.isMenuActive = false)
-            ]
-          }
-        });
-      }
+      });
     }
   });
 </script>
