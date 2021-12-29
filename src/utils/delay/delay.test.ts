77f7347c4360526e181e28b0ab6917e9e68decd8
Zhang-Wei-666 <994039348@qq.com>
2021-12-29 17:09:29 +0800

    新增 delay 工具方法

---
+++ src/utils/delay/delay.test.ts
@@ -0,0 +1,25 @@
+import { test, expect } from 'vitest'; // eslint-disable-line import/no-extraneous-dependencies
+import { delay } from './delay';
+
+
+test('delay: 返回一个 Promise', () => {
+  expect(delay()).toBeInstanceOf(Promise);
+});
+
+test('delay: 默认延迟时间为 1000', async () => {
+  const start = Date.now();
+  await delay();
+  const end = Date.now();
+  expect(end - start).toBeGreaterThanOrEqual(1000);
+});
+
+test('delay: 指定延迟时间', async () => {
+  const times = [1, 10, 20, 100, 200, 1000, 2000];
+
+  for (const time of times) {
+    const start = Date.now();
+    await delay(time); // eslint-disable-line no-await-in-loop
+    const end = Date.now();
+    expect(end - start).toBeGreaterThanOrEqual(time);
+  }
+});
---
+++ src/utils/delay/delay.ts
@@ -0,0 +1,9 @@
+/**
+ * 返回一个延迟指定时间的 Promise
+ * @param ms 延迟时间 ( default: 1000 )
+ */
+export function delay(ms: number = 1000) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
