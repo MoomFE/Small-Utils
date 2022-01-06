f6e04014adc7a09c1dcaa87a0a4c71f679223f67
Zhang-Wei-666 <994039348@qq.com>
2022-01-06 17:34:42 +0800

    调整打包相关配置

--- scripts/rollup.config.ts
+++ scripts/rollup.config.ts
@@ -8,52 +8,58 @@ import esbuild from 'rollup-plugin-esbuild';
 import dts from 'rollup-plugin-dts';
 
 
-const configs: RollupOptions[] = [{
-  input: 'src/index.ts',
-  output: [
-    {
-      file: 'index.mjs',
-      format: 'es'
-    },
-    {
-      file: 'index.cjs',
-      format: 'cjs'
-    }
-  ],
-  plugins: [
-    {
-      name: 'alias',
-      resolveId(importee) {
-        if (importee.startsWith('@/')) {
-          const path = resolve(__dirname, '../src', importee.slice(2));
-          const paths = [
-            resolve(path, 'index.ts'),
-            resolve(path, 'index.js'),
-            path
-          ];
+const rollupAliasPlugin = {
+  name: 'alias',
+  resolveId(importee) {
+    if (importee.startsWith('@/')) {
+      const path = resolve(__dirname, '../src', importee.slice(2));
+      const paths = [
+        resolve(path, 'index.ts'),
+        resolve(path, 'index.js'),
+        path
+      ];
 
-          for (const p of paths) {
-            if (fs.pathExistsSync(p)) return p;
-          }
-        }
+      for (const p of paths) {
+        if (fs.pathExistsSync(p)) return p;
       }
-    },
-    esbuild({
-      tsconfig: resolve(__dirname, '../tsconfig.json'),
-    })
-  ]
-}];
+    }
+  }
+};
+
+const rollupEsbuildPlugin = esbuild({
+  tsconfig: resolve(__dirname, '../tsconfig.json'),
+});
+
+const rollupDtsPlugin = dts();
 
 
-configs.push({
-  input: 'src/index.ts',
-  output: [{
-    file: 'types/index.d.ts',
-    format: 'es'
-  }],
-  plugins: [
-    dts()
-  ]
+const configs: RollupOptions[] = [];
+
+
+['utils', 'validator'].forEach((name) => {
+  //
+  configs.push({
+    input: `src/${name}/index.ts`,
+    output: [
+      { file: `${name}/index.mjs`, format: 'es' },
+      { file: `${name}/index.cjs`, format: 'cjs' }
+    ],
+    plugins: [
+      rollupAliasPlugin,
+      rollupEsbuildPlugin
+    ]
+  });
+  //
+  configs.push({
+    input: `src/${name}/index.ts`,
+    output: [{
+      file: `${name}/index.d.ts`,
+      format: 'es'
+    }],
+    plugins: [
+      rollupDtsPlugin
+    ]
+  });
 });
 
 
