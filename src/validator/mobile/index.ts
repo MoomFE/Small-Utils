d17f6a5ca1a7b02e6fe28431e4be3556b5034237
Zhang-Wei-666 <994039348@qq.com>
2022-01-06 11:13:18 +0800

    减少不必要的导出, 完善单元测试

--- src/validator/mobile/index.ts
+++ src/validator/mobile/index.ts
@@ -1,16 +1,7 @@
 /**
- * 判断字符串是否包含移动电话号码的正则字符串
- */
-export const hasMobileRegStr = '1[3-9]\\d{9}';
-
-/**
- * 判断字符串是否是移动电话号码的正则字符串
- */
-export const isMobileRegStr = `^${hasMobileRegStr}$`;
-/**
  * 判断字符串是否是移动电话号码的正则
  */
-export const isMobileReg = new RegExp(isMobileRegStr);
+const isMobileReg = /^1[3-9]\d{9}$/;
 
 
 /**
