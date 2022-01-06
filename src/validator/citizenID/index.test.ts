98bd1cb34b2a22a294c931f15586526d85499762
Zhang-Wei-666 <994039348@qq.com>
2022-01-06 13:51:42 +0800

    更新身份证号码的正则, 完善单元测试

--- src/validator/citizenID/index.test.ts
+++ src/validator/citizenID/index.test.ts
@@ -10,3 +10,11 @@ test('isCitizenID: 基础测试', () => {
   expect(isCitizenID('36060999999999999x')).toBe(false);
   expect(isCitizenID('36060999999999999X')).toBe(false);
 });
+
+test('isCitizenID: 身份证中的年份测试', () => {
+  expect(isCitizenID('360602179901239999')).toBe(false);
+  expect(isCitizenID('360602189901239999')).toBe(true);
+  expect(isCitizenID('360602199901239999')).toBe(true);
+  expect(isCitizenID('360602209901239999')).toBe(true);
+  expect(isCitizenID('360602219901239999')).toBe(false);
+});
