d985aea3ad2889d71ef33673fbc093481c112871
倚窗听雨丿醉无心 <994039348@qq.com>
2022-02-18 17:17:16 +0800

    优化类型定义

--- src/utils/deepUnref/index.ts
+++ src/utils/deepUnref/index.ts
@@ -1,4 +1,4 @@
-import type { Ref } from 'vue-demi';
+import type { Ref, UnwrapNestedRefs } from 'vue-demi';
 import { unref } from 'vue-demi';
 import { isPlainObject } from '@/utils';
 
@@ -8,14 +8,14 @@ import { isPlainObject } from '@/utils';
  * 如果传入的是普通对象, 会不断向下查找然后解包并返回传入对象的副本,
  * 否则直接返回传入值的 `unref` 结果.
  */
-export function deepUnref<T>(maybeRef: T | Ref<T>): T {
+export function deepUnref<T>(maybeRef: T | Ref<T>): UnwrapNestedRefs<T> {
   const value = unref(maybeRef);
 
   if (isPlainObject(value)) {
     return Object.fromEntries(
       Object.entries(value).map(([k, v]) => [k, deepUnref(v)])
-    ) as T;
+    ) as UnwrapNestedRefs<T>;
   }
 
-  return value;
+  return value as UnwrapNestedRefs<T>;
 }
