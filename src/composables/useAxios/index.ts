830fb3858b1b8b25f4efc2b52803384cd503bbb5
倚窗听雨丿醉无心 <994039348@qq.com>
2022-02-18 18:08:15 +0800

    开始封装 useAxios 组合式方法

---
+++ src/composables/useAxios/index.ts
@@ -0,0 +1,7 @@
+import type { MaybeRef } from '@vueuse/core';
+import type { AxiosRequestConfig } from 'axios';
+
+
+export function useAxios(url: MaybeRef<string>, config: MaybeRef<AxiosRequestConfig>) {
+
+}
