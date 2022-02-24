1c3caf32f5b772fd56272a796103600aad1afd92
倚窗听雨丿醉无心 <994039348@qq.com>
2022-02-24 11:23:57 +0800

    修改组件目录结构

--- src/components/responsive/index.ts
+++ src/components/responsive/index.ts
@@ -1,1 +1,78 @@
-export * from './responsive';
+import { h, computed, defineComponent } from 'vue-demi';
+import CssRender from 'css-render';
+
+
+/** 当前浏览器是否支持纵横比属性 */
+const supportsAspectRatio = CSS.supports('aspect-ratio: 1');
+
+
+const responsiveProps = {
+  /** ( 宽度 / 高度 ) 计算的基本长宽比 */
+  aspectRatio: {
+    type: [String, Number],
+    default: 1
+  },
+  /** 尺寸调整容器样式类 */
+  sizerClass: String,
+  /** 内容容器样式类 */
+  contentClass: String,
+};
+
+
+export type ResponsiveProps = typeof responsiveProps;
+export const SResponsive = defineComponent({
+  name: 's-responsive',
+  props: responsiveProps,
+  setup(props, { slots }) {
+    /** 数字格式长宽比 */
+    const computedAspectRatio = computed(() => Number(props.aspectRatio));
+    /** 尺寸调整容器样式 */
+    const sizerStyle = computed(() => {
+      if (!supportsAspectRatio && computedAspectRatio.value) {
+        return {
+          paddingBottom: `${(1 / computedAspectRatio.value) * 100}%`
+        };
+      }
+      return undefined;
+    });
+    /** 内容容器样式 */
+    const contentStyle = computed(() => {
+      if (supportsAspectRatio && computedAspectRatio.value) {
+        return {
+          aspectRatio: `${computedAspectRatio.value}`
+        };
+      }
+      return undefined;
+    });
+
+    return () => {
+      return h('div', { class: 's-responsive' }, [
+        sizerStyle.value && h('div', {
+          class: ['s-responsive-sizer', props.sizerClass],
+          style: sizerStyle.value
+        }),
+        h('div', {
+          class: ['s-responsive-content', props.contentClass],
+          style: contentStyle.value
+        }, slots.default?.())
+      ]);
+    };
+  }
+});
+
+
+const { c } = CssRender();
+const style = c('.s-responsive', {
+  maxWidth: '100%',
+  display: 'flex',
+  flex: '1 0 auto',
+  position: 'relative',
+  overflow: 'hidden'
+}, [
+  c('.s-responsive-sizer', { flex: '1 0 0px' }, [
+    c('~ .s-responsive-content', { marginLeft: '-100%' })
+  ]),
+  c('.s-responsive-content', { maxWidth: '100%', flex: '1 0 0px' })
+]);
+
+style.mount();
--- src/components/scrollbars/index.ts
+++ src/components/scrollbars/index.ts
@@ -1,1 +1,85 @@
-export * from './scrollbars';
+import {
+  h,
+  ref, watchEffect,
+  onMounted, onUnmounted,
+  defineComponent
+} from 'vue-demi';
+import { templateRef } from '@vueuse/core';
+import 'overlayscrollbars/css/OverlayScrollbars.css';
+import 'overlayscrollbars/js/OverlayScrollbars.min';
+
+
+const scrollbarsProps = {
+  /** 传递给 overlayscrollbars 的 options */
+  options: Object,
+  /** 传递给 overlayscrollbars 的 extensions */
+  extensions: [String, Array, Object],
+  /** 组件 padding 层样式类 */
+  paddingClass: String,
+  /** 组件 viewport 层样式类 */
+  viewportClass: String,
+  /** 组件 content 层样式类 */
+  contentClass: String
+};
+
+export type ScrollbarsProps = typeof scrollbarsProps;
+export const SScrollbars = defineComponent({
+  name: 's-scrollbars',
+  props: scrollbarsProps,
+  setup(props, { slots }) {
+    const osInstace = ref();
+    const el = templateRef<HTMLDivElement | null>('el');
+
+    onMounted(() => {
+      // @ts-ignore
+      osInstace.value = window.OverlayScrollbars(
+        el.value,
+        props.options || {},
+        props.extensions
+      );
+    });
+
+    watchEffect(() => {
+      // @ts-ignore
+      if (window.OverlayScrollbars.valid(osInstace.value)) {
+        osInstace.value.options(props.options);
+      }
+    });
+
+    onUnmounted(() => {
+      // @ts-ignore
+      if (window.OverlayScrollbars.valid(osInstace.value)) {
+        osInstace.value.destroy();
+        osInstace.value = null;
+      }
+    });
+
+    return {
+      el,
+      slots,
+      scroll: (...args: any[]) => osInstace.value?.scroll(...args),
+      scrollStop: (...args: any[]) => osInstace.value?.scrollStop(...args),
+    };
+  },
+  render() {
+    return h('div', { ref: 'el', class: 'os-host s-scrollbars' }, [
+      h('div', { class: 'os-resize-observer-host' }),
+      h('div', { class: `os-padding ${this.paddingClass || ''}` }, [
+        h('div', { class: `os-viewport ${this.viewportClass || ''}` }, [
+          h('div', { class: `os-content ${this.contentClass || ''}` }, this.slots.default?.())
+        ])
+      ]),
+      h('div', { class: 'os-scrollbar os-scrollbar-horizontal' }, [
+        h('div', { class: 'os-scrollbar-track' }, [
+          h('div', { class: 'os-scrollbar-handle' })
+        ])
+      ]),
+      h('div', { class: 'os-scrollbar os-scrollbar-vertical' }, [
+        h('div', { class: 'os-scrollbar-track' }, [
+          h('div', { class: 'os-scrollbar-handle' })
+        ])
+      ]),
+      h('div', { class: 'os-scrollbar-corner' })
+    ]);
+  }
+});
