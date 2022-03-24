85e49305c15a254943ae9cf5d82e864f50e5a066
倚窗听雨丿醉无心 <994039348@qq.com>
2022-03-24 16:59:16 +0800

    优化 leastRun 方法

--- src/utils/leastRun/index.ts
+++ src/utils/leastRun/index.ts
@@ -1,5 +1,4 @@
-import { delay } from '@/utils';
-
+import { delay, isPromise } from '@/utils';
 
 /**
  * 运行函数并且保证最少执行指定 ms 的时间
@@ -11,8 +10,9 @@ export async function leastRun(ms: number, fn: Function): Promise<any>;
  * 运行函数并且保证最少执行 1000ms 的时间
  * @param fn 要运行的函数
  */
-export async function leastRun(fn: Function): Promise<any>; // eslint-disable-line no-redeclare
-export async function leastRun(msOrFn: number | Function, maybeFn?: Function) { // eslint-disable-line no-redeclare
+export async function leastRun(fn: Function): Promise<any>;
+
+export async function leastRun(msOrFn: number | Function, maybeFn?: Function) {
   const start = Date.now();
   let ms = msOrFn as number;
   let fn = maybeFn || (() => {});
@@ -25,9 +25,8 @@ export async function leastRun(msOrFn: number | Function, maybeFn?: Function) { 
   let result = fn();
 
   // 如果函数是异步函数, 那么等待函数执行完毕
-  if (result instanceof Promise) {
+  if (isPromise(result))
     result = await result;
-  }
 
   // 保证函数至少执行了 ms 毫秒
   await delay(Math.max(0, ms - (Date.now() - start)));
