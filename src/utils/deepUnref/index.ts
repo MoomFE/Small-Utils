1a3b959138802c13cf5a916559491cbe7e84385d
倚窗听雨丿醉无心 <994039348@qq.com>
2022-03-29 13:25:08 +0800

    注释格式修改

--- src/utils/deepUnref/index.ts
+++ src/utils/deepUnref/index.ts
@@ -4,8 +4,7 @@ import { isPlainObject } from '@/utils';
 
 /**
  * Vue `unref` 的加强版本,
- * 如果传入的是普通对象和数组, 会不断向下查找然后解包并返回传入对象的副本,
- * 否则直接返回传入值的 `unref` 结果.
+ *  - 如果传入的是普通对象和数组, 会不断向下查找然后解包并返回传入对象的副本, 否则直接返回传入值的 `unref` 结果.
  */
 export function deepUnref<T>(maybeRef: T | Ref<T>): UnwrapNestedRefs<T> {
   const value = unref(maybeRef);
