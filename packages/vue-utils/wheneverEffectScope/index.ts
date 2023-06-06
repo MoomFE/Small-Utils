259d3925a214ff9d2974350713848d7f01e1d928
倚窗听雨丿醉无心 <994039348@qq.com>
2023-06-06 10:23:30 +0800

    优化 `wheneverEffectScope` 工具方法类型定义

--- packages/vue-utils/wheneverEffectScope/index.ts
+++ packages/vue-utils/wheneverEffectScope/index.ts
@@ -5,15 +5,7 @@ import { effectScope, watch } from 'vue-demi';
  * 监听传入值为 truthy 时, 创建一个 effect 作用域
  * 当值变为 falsy 时, 将会停止之前创建的 effect 作用域
  */
-export function wheneverEffectScope<
-T,
-V extends T | false | null | undefined,
-Immediate extends Readonly<boolean> = false,
->(
-  source: WatchSource<V>,
-  run: (...args: Parameters<WatchCallback<V>>) => void,
-  options?: WatchOptions<Immediate>,
-): WatchStopHandle {
+export function wheneverEffectScope<T>(source: WatchSource<T | false | null | undefined>, run: WatchCallback<T>, options?: WatchOptions): WatchStopHandle {
   let scope: EffectScope | void;
 
   const dispose = () => {
